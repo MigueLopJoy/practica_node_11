b4aabd739cea57fe689d92afa1080a3d4a1eed3d
= <m.lop.rod.97@gmail.com>
2024-01-26 09:27:54 +0100

    Práctica 11 terminada

--- server.js
+++ server.js
@@ -1,60 +1,31 @@
 const server = require('http')
-const url = require('url')
 const files = require('fs')
-const path = require('path')
-const querystring = require('querystring')
-
 
 server.createServer((req, res) => {
     switch (req.url) {
         case "/":
         case "/home":
             files.readFile("index.html", (err, data) => {
-                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.writeHead(200, { 'Content-Type': 'text/html' })
                 res.write(data)
                 res.end("")
             })
             break
         case "/contact":
-            if (req.method === 'GET') {
-                files.readFile('contact.html', (err, data) => {
-                    res.writeHead(200, { 'Content-Type': 'text/html' })
-                    res.write(data)
-                    res.end('')
-                });
-            } else if (req.method === 'POST') {
-                let body = ''
-                req.on('data', (chunk) => {
-                    body += chunk
-                })
-                req.on('end', () => {
-                    const formData = querystring.parse(body),       
-                        dataString = `Nombre: ${formData.name}\nCorreo: ${formData.email}\nMensaje: ${formData.message}\n`
-                
-                    files.appendFile('datos.txt', dataString, (err) => {
-                    if (err) {
-                        console.error(err)
-                        res.writeHead(500, { 'Content-Type': 'text/html' })
-                        res.write('Error interno del servidor')
-                        res.end('');
-                    } else {
-                        files.readFile('contact.html', (err, data) => {
-                            res.writeHead(200, { 'Content-Type': 'text/html' })
-                            res.write(data)
-                            res.end('')
-                        })
-                    }
-                })
-            }
+            files.readFile('contact.html', (err, data) => {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.write(data)
+                res.end('')
+            });
             break
         default:
-                files.readFile("notfound.html", (err, data) => {
-                    res.writeHead(200, {'Content-Type': 'text/html'}) 
-                    res.write(data)
-                    res.end("")
-                })           
+            files.readFile("notfound.html", (err, data) => {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.write(data)
+                res.end("")
+            })
             break
     }
-}).listen(80, () => {
-    console.log('Servidor escuchando en http://localhost:80')
+}).listen(8080, () => {
+    console.log('Servidor escuchando en http://localhost:8080')
 })
